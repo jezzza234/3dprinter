2fd8e5961d61d4090927badf5e5b3d06055e37f4
jezzza234 <jezza@pobox.com>
2023-11-23 14:01:37 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@ if [ ! -d "~/Downloads" ]; then
     mkdir "~/Downloads"
 fi
 
+sudo apt install libfuse2 -y
+
 wget https://github.com/Ultimaker/Cura/releases/download/5.5.0/UltiMaker-Cura-5.5.0-linux-X64.AppImage -O ~/Downloads/cura.AppImage
 chmod 755 ~/Downloads/cura.AppImage
 ~/Downloads/cura.AppImage &
